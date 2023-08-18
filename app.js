fcfc6dcd6abc48fb791b25a6eaae6c9129bbc80d
Matija Patekar <patekar.matija@gmail.com>
2023-08-19 00:00:33 +0200

    level 4a security: hashing and salting end

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const md5 = require("md5"); //we added md5 (hashing) for data encryption
+const bcrypt = require("bcrypt"); //we added bcrypt (hashing) for data encryption
+const saltRounds = 10;  //we added saltRounds for data encryption
 
-//start adding mongoose encryption level4a: hashing and salting
+//start adding mongoose encryption level4a: hashing and salting (using bcrypt)
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -41,37 +42,63 @@ app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
 
-//when the user enters their username and passwors and clicks the submit button, 
-//the form will be submitted to the server
-//using post reguest to the register route (See register.ejs)
+/* In the register route, bcrypt.hash is used to hash the password with a specified number of salt rounds 
+(which increases the security of the hashing process). The resulting hashed password is then stored
+ in the newUser object and saved to the database. */
 
 app.post("/register", async function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)  //we used md5 to encrypt the password
-    });
+    const plainPassword = req.body.password;
 
-    //save the user
-    //only if the new user is registered, he can see the secrets page
-   try{
-    await newUser.save();
-    res.render("secrets");
-   } catch(err){
-       console.log(err);
-   }
+    // Hash the password using bcrypt
+    try {
+        const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hashedPassword
+        });
+        // Save the user
+        await newUser.save();
+        res.render("secrets");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("An error occurred while registering.");
+    }
 });
+
+
+/* in the login route, bcrypt.compare is used to compare the plain password provided in the login request 
+with the hashed password stored in the database for the corresponding user. If the passwords match, 
+the user is logged in and can access the secrets page. If the passwords don't match or the user is not found,
+ appropriate error responses are sent. */
 
 app.post("/login", async function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);  //we have to use md5 here also (to compare with the password from the register route)
-    try{
-        const foundUser = await User.findOne({email: username});
-        if(foundUser){
-            if(foundUser.password === password){
+    const plainPassword = req.body.password;
+
+    try {
+        const foundUser = await User.findOne({ email: username });
+
+        if (foundUser) {
+            // Compare the hashed password with the plain password using bcrypt.compare
+            const passwordsMatch = await bcrypt.compare(plainPassword, foundUser.password);
+
+            if (passwordsMatch) {
                 res.render("secrets");
+            } else {
+                res.status(401).send("Incorrect password");
             }
+        } else {
+            res.status(404).send("User not found");
         }
-    } catch(err){
+    } catch (err) {
         console.log(err);
+        res.status(500).send("An error occurred while logging in.");
     }
-})
+});
+
+
+
+
+
+
