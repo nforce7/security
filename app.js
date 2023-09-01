33f0b14a1a4cf5b5bae0729776f85415f6251713
Matija Patekar <patekar.matija@gmail.com>
2023-09-02 01:03:03 +0200

    level 5 security: passport with cookies and sessions end

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt"); 
-const saltRounds = 10;  
+const session = require("express-session"); //the first step is to require express-session
+const passport = require("passport"); //the second step is to require passport
+const passportLocalMongoose = require("passport-local-mongoose"); //the third step is to require passport-local-mongoose
 
-//start adding mongoose encryption level5 : passport using cookies and sessions
+//start adding mongoose encryption level5 : passport using cookies and sessions.
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//the fourth step is to set up the session
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//the next step is to require passport and to initialize it
+app.use(passport.initialize());
+app.use(passport.session());
 
 //setup mongoose, userSchema and Model 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, useunifiedTopology: true});
@@ -20,7 +32,18 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+//the fifth step is to set up passport-local-mongoose
+//we have to add it to our mongoose schema (userSchema) as a plugin
+//we use this to salt and hash our passwords and save users to our database
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+//the next step is to initialize passport. This code must go after the mongoose model
+passport.use(User.createStrategy());
+//serialize and deserialize is only needed for sessions
+passport.serializeUser(User.serializeUser());  //to store the user in the session
+passport.deserializeUser(User.deserializeUser()); //to retrieve the user from the session
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -38,63 +61,58 @@ app.get("/compose", function(req, res) {
     res.render("compose");
 })
 
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+})
+
+app.get("/logout", function(req, res) {
+    req.logout(function(err) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+      // Redirect to the home page or any other page after successful logout
+      res.redirect("/");
+    });
+  });
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
 
-/* In the register route, bcrypt.hash is used to hash the password with a specified number of salt rounds 
-(which increases the security of the hashing process). The resulting hashed password is then stored
- in the newUser object and saved to the database. */
-
+//the next step is to set up the routes :register and login
 app.post("/register", async function(req, res) {
-    const plainPassword = req.body.password;
-
-    // Hash the password using bcrypt
-    try {
-        const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
-
-        const newUser = new User({
-            email: req.body.username,
-            password: hashedPassword
-        });
-        // Save the user
-        await newUser.save();
-        res.render("secrets");
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("An error occurred while registering.");
-    }
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");  //if authentification is successful, we redirect the user to the secrets page
+            })
+        }
+    })
 });
 
-
-/* in the login route, bcrypt.compare is used to compare the plain password provided in the login request 
-with the hashed password stored in the database for the corresponding user. If the passwords match, 
-the user is logged in and can access the secrets page. If the passwords don't match or the user is not found,
- appropriate error responses are sent. */
-
 app.post("/login", async function(req, res) {
-    const username = req.body.username;
-    const plainPassword = req.body.password;
-
-    try {
-        const foundUser = await User.findOne({ email: username });
-
-        if (foundUser) {
-            // Compare the hashed password with the plain password using bcrypt.compare
-            const passwordsMatch = await bcrypt.compare(plainPassword, foundUser.password);
-
-            if (passwordsMatch) {
-                res.render("secrets");
-            } else {
-                res.status(401).send("Incorrect password");
-            }
-        } else {
-            res.status(404).send("User not found");
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("An error occurred while logging in.");
-    }
+   //if user is registered, we authenticate the user
+   const user = new User({
+       username: req.body.username,
+       password: req.body.password
+   })
+   req.login(user, function(err) {
+       if (err) {
+           console.log(err);
+       } else {
+           passport.authenticate("local")(req, res, function() {
+               res.redirect("/secrets");
+           })
+       }
+   })
 });
 
 
